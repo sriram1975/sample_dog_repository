f5a4290e537ea32d17fab4d3cde7a6eb392f0ff1
sriram1975 <indianandhra1980@gmail.com>
2024-05-27 18:57:30 +0530

    DogDaoImplTest.java updated with test cases

--- sample_dog_project/datasource/src/test/java/com/acme/datasource/dao/DogDaoImplTest.java
+++ sample_dog_project/datasource/src/test/java/com/acme/datasource/dao/DogDaoImplTest.java
@@ -1,11 +1,20 @@
 package com.acme.datasource.dao;
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.function.Supplier;
 
+import com.acme.datasource.model.Dog;
+import com.acme.datasource.model.Gender;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class DogDaoImplTest {
@@ -15,33 +24,49 @@ class DogDaoImplTest {
 
 	@Test
 	void createTest() throws SQLException {
-
-		
+		Dog dog = new Dog();
+		dog.setId("111");
+		dog.setName("Jimmy");
+		SimpleDateFormat date = new SimpleDateFormat();
+		dog.setBirthDate(LocalDate.of(2020, 12, 12));
+		dog.setSpecies("Siberian Husky");
+		dog.setGender(Gender.M);
+		assertTrue(dogDao.create(dog));
 	}
 
 	@Test
 	void updateTest() throws SQLException {
-		// TODO
+		Dog dog = new Dog();
+		dog.setId("111");
+		dog.setName("Jimmy");
+		dog.setBirthDate(LocalDate.of(2020, 12, 12));
+		dog.setSpecies("Saint Bernard");
+		dog.setGender(Gender.M);
+		assertTrue(dogDao.update(dog));
 	}
 
 	@Test
 	void readByIdTest() throws SQLException {
-		// TODO
+		Optional<Dog> optionalDog = dogDao.readById("111");
+		Dog dog = optionalDog.orElseThrow();
+		assertEquals("111", dog.getId());
+		assertEquals("Saint Bernard", dog.getSpecies());
 	}
 
 	@Test
 	void readAllTest() throws SQLException {
-		// TODO
+		List<Dog> dogList = dogDao.readAll();
+		assertEquals(6, dogList.size());
 	}
 
 	@Test
 	void readByBirthDateGreaterThanTest() throws SQLException {
-		// TODO
+		List<Dog> dogList = dogDao.readByBirthDateGreaterThan(LocalDate.of(2018, 12, 12));
+		assertEquals(3, dogList.size());
 	}
 
 	@Test
 	void deleteTest() throws SQLException {
-		// TODO
+		assertTrue(dogDao.delete("111"));
 	}
-
 }
