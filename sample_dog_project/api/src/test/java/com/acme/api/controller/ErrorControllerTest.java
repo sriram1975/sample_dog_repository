99854ec534e0f4f57a81d63f8817a035a021bdac
sriram1975 <indianandhra1980@gmail.com>
2024-05-28 14:30:31 +0530

    ErrorControllerTest.java updated with JUNIT test case

--- sample_dog_project/api/src/test/java/com/acme/api/controller/ErrorControllerTest.java
+++ sample_dog_project/api/src/test/java/com/acme/api/controller/ErrorControllerTest.java
@@ -1,7 +1,20 @@
 package com.acme.api.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ErrorControllerTest {
 
-	// TODO
+    public ErrorController errorController = new ErrorController();
 
+    @Test
+    public void handleThrowableTest()
+    {
+        ResponseEntity<String> responseEntity = errorController.handleThrowable(new Throwable());
+        assertEquals("500", Objects.requireNonNull(responseEntity.getStatusCode()).toString());
+    }
 }
