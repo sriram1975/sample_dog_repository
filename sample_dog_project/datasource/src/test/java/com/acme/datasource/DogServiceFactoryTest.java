a8c2d499bde19d34af8e07af92517741fb817f0e
sriram1975 <indianandhra1980@gmail.com>
2024-05-28 10:21:22 +0530

    DogServiceFactoryTest.java updated with JUNIT test case

--- sample_dog_project/datasource/src/test/java/com/acme/datasource/DogServiceFactoryTest.java
+++ sample_dog_project/datasource/src/test/java/com/acme/datasource/DogServiceFactoryTest.java
@@ -2,13 +2,26 @@ package com.acme.datasource;
 
 import java.net.URISyntaxException;
 
+import com.acme.datasource.dao.DogDao;
+import com.acme.datasource.dao.DogDaoImpl;
+import com.acme.datasource.impl.DogDatasourceImpl;
+import com.acme.datasource.reader.DogReader;
+import com.acme.datasource.reader.impl.DogReaderImpl;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DogDatasourceFactoryTest {
 
 	@Test
 	void createTest() throws URISyntaxException {
-		// TODO
+		DogDatasource dogDatasource = DogDatasourceFactory.create();
+
+		DogReader dogReader = new DogReaderImpl();
+		DogDao dogDao = new DogDaoImpl();
+		DogDatasourceImpl dogDatasourceImpl = new DogDatasourceImpl(dogReader, dogDao);
+		dogDatasourceImpl.init();
+		assertEquals(dogDatasourceImpl, dogDatasource);
 	}
 
 }
