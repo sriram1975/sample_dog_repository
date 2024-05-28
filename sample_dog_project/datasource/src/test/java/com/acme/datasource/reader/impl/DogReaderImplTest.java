38f7c9d281fc2b863ae9a64ceb1eba6e1a18e00f
sriram1975 <indianandhra1980@gmail.com>
2024-05-28 10:31:59 +0530

    DogReaderImplTest.java updated with JUNIT test case

--- sample_dog_project/datasource/src/test/java/com/acme/datasource/reader/impl/DogReaderImplTest.java
+++ sample_dog_project/datasource/src/test/java/com/acme/datasource/reader/impl/DogReaderImplTest.java
@@ -1,14 +1,20 @@
 package com.acme.datasource.reader.impl;
 
 import java.net.URISyntaxException;
+import java.util.List;
 
+import com.acme.datasource.model.Dog;
+import com.acme.datasource.reader.DogReader;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DogReaderImplTest {
 
 	@Test
 	void readTest() throws URISyntaxException {
-		// TODO
+		DogReader dogReader = new DogReaderImpl();
+		List<Dog> dogList = dogReader.read();
+		assertEquals(5, dogList.size());
 	}
-
 }
